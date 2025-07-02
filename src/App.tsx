f2a0afef0ce14c860d7a68f954188a54e29c9d6c
VlShost <vlshost9386@gmail.com>
2025-07-03 00:14:07 +0300

    added header and shared layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,33 @@ import Dashboard from './pages/Dashboard';
 import Companies from './pages/Companies';
 import PublicRoute from './router/PublicRoute';
 import PrivateRoute from './router/PrivateRoute';
+import Layout from './components/SharedLayout/Layout/Layout';
 
 function App() {
   return (
     <Routes>
-      <Route
-        path="auth/signup"
-        element={<PublicRoute redirectTo="/companies" component={<SignUp />} />}
-      />
-      <Route
-        path="auth/signin"
-        element={<PublicRoute redirectTo="/companies" component={<SignIn />} />}
-      />
-      <Route
-        path="/profile"
-        element={<PrivateRoute redirectTo="/auth/signin" component={<Profile />} />}
-      />
-      <Route
-        path="/dashboard"
-        element={<PrivateRoute redirectTo="/auth/signin" component={<Dashboard />} />}
-      />
-      <Route
-        path="/companies"
-        element={<PrivateRoute redirectTo="/auth/signin" component={<Companies />} />}
-      />
+      <Route path="/" element={<Layout />}>
+        <Route
+          path="auth/signup"
+          element={<PublicRoute redirectTo="/companies" component={<SignUp />} />}
+        />
+        <Route
+          path="auth/signin"
+          element={<PublicRoute redirectTo="/companies" component={<SignIn />} />}
+        />
+        <Route
+          path="/profile"
+          element={<PrivateRoute redirectTo="/auth/signin" component={<Profile />} />}
+        />
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute redirectTo="/auth/signin" component={<Dashboard />} />}
+        />
+        <Route
+          path="/companies"
+          element={<PrivateRoute redirectTo="/auth/signin" component={<Companies />} />}
+        />
+      </Route>
     </Routes>
   );
 }
