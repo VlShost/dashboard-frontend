b31a0b42953a36876c320477e0286e538a90f0bd
VlShost <vlshost9386@gmail.com>
2025-07-03 00:15:27 +0300

    small fix

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
 import SignInForm from '../components/Forms/SignInForm/SignInForm';
 
 const SignIn = () => {
   return (
     <div>
-      SignIn
       <SignInForm />
     </div>
   );
