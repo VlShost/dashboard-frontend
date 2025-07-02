f27cebeec164ce8e9ec226f47c83802b18ea9a01
VlShost <vlshost9386@gmail.com>
2025-07-02 11:52:40 +0300

    created base sign in form component

--- src/components/Forms/SignInForm/SignInForm.tsx
+++ src/components/Forms/SignInForm/SignInForm.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const SignInForm = () => {
-  return <div>SignInForm</div>;
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log('form submitted');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Sign In</h2>
+
+      <div>
+        <label>
+          <input type="email" placeholder="Email" />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          <input type="password" placeholder="Password" />
+        </label>
+      </div>
+
+      <button type="submit">Sign In</button>
+    </form>
+  );
 };
 
 export default SignInForm;
