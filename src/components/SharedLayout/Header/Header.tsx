f055aa197ff29f96c2241131ac58550155151881
VlShost <vlshost9386@gmail.com>
2025-07-03 02:33:33 +0300

    added header styles and components

--- src/components/SharedLayout/Header/Header.tsx
+++ src/components/SharedLayout/Header/Header.tsx
@@ -1,5 +1,34 @@
+import { NavLink } from 'react-router-dom';
+import css from './Header.module.scss';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={css.header}>
+      <nav className={css.nav}>
+        <ul className={css.navList}>
+          <li className={css.navItem}>
+            <NavLink to="/auth/signin">Sign In</NavLink>
+          </li>
+
+          <li className={css.navItem}>
+            <NavLink to="/auth/signup">Sign Up</NavLink>
+          </li>
+
+          <li className={css.navItem}>
+            <NavLink to="/companies">Companies</NavLink>
+          </li>
+
+          <li className={css.navItem}>
+            <NavLink to="/profile">Profile</NavLink>
+          </li>
+
+          <li className={css.navItem}>
+            <NavLink to="/dashboard">Dashboard</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
