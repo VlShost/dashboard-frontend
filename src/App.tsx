543f17c69444fb9b571d0c237255a89f9269d6fe
VlShost <vlshost9386@gmail.com>
2025-07-02 11:54:23 +0300

    added more routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import SignIn from './pages/SignIn';
+
 import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import Dashboard from './pages/Dashboard';
+import Companies from './pages/Companies';
 
 function App() {
   return (
     <Routes>
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
-      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-      {/* <Route path="/companies" element={<Companies />} /> */}
+      <Route path="auth/signup" element={<SignUp />} />
+      <Route path="auth/signin" element={<SignIn />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/companies" element={<Companies />} />
     </Routes>
   );
 }
